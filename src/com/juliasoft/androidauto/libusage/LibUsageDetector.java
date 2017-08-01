dab4eca3e7f631fe8c4007cace33d34e66142d67
Pietro Ferrara <pietro.ferrara@juliasoft.com>
2017-08-01 15:06:50 +0200

    more dump information, csv files

--- src/com/juliasoft/androidauto/libusage/LibUsageDetector.java
+++ src/com/juliasoft/androidauto/libusage/LibUsageDetector.java
@@ -1,7 +1,6 @@
 package com.juliasoft.androidauto.libusage;
 
 
-import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -12,11 +11,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.jar.JarEntry;
 import java.util.jar.JarInputStream;
-import java.util.stream.Stream;
 
 import org.apache.bcel.classfile.*;
 import org.apache.bcel.generic.*;
@@ -28,6 +28,9 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionGroup;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -48,6 +51,7 @@ public class LibUsageDetector {
 			String pckge = normalizePackage(line.getOptionValue("p"));
 			String app = line.getOptionValue("a");
 			String o = line.getOptionValue("o");
+			//Processing the application or the directory
 			if(o!=null)
 				output = o+File.separatorChar;
 			else {
@@ -73,6 +77,7 @@ public class LibUsageDetector {
 		return "L"+packge.replace(".", "/");
 	}
 
+	//Analyze all the jar files in the directory
 	private static void analyzeDir(String dir, String pckge) {
 		Arrays.stream(new File(dir).listFiles()).forEach(app -> {
 			try {
@@ -84,12 +89,14 @@ public class LibUsageDetector {
 		});
 	}
 
+	//true iff we should consider a file with the given extension
 	private static boolean isFileTypeConsidered(String fileExtension) {
 		return fileExtension.equals("jar");
 	}
-
+	
+	//Analyze a single application
 	private static void analyzeSingleApp(String app, String pckge) throws IOException, FileNotFoundException {
-		Collection<MethodGen> result = containsCallToPackage(app, pckge);
+		Collection<Pair<String, MethodGen>> result = containsCallToPackage(app, pckge);
 		if(result.isEmpty())
 			logger.info("The app "+app+" does not contain any call to package "+pckge);
 		else {
@@ -100,6 +107,7 @@ public class LibUsageDetector {
 		}
 	}
 
+	//Options of the command line tool
 	private static Options getOptions() {
 		Options options = new Options();
 		OptionGroup group = new OptionGroup();
@@ -112,48 +120,65 @@ public class LibUsageDetector {
 		return options;
 	}
 
+	//Standard name of the file where the results will be dumped
 	private static String getLoggerFileName(String app, String pckge) {
-		return app+"_"+pckge.substring(1).replace("/", ".")+".log";
+		return app+"_"+pckge.substring(1).replace("/", ".")+".csv";
 	}
 
-	private static boolean dumpToFile(Collection<MethodGen> result, String app) {
+	//Dump the results to a csv file
+	private static boolean dumpToFile(Collection<Pair<String, MethodGen>> result, String app) {
+		Object[] FILE_HEADER = {"Application Method","Called API"};
 		File logFile = new File(output+Files.getNameWithoutExtension(app)+"."+Files.getFileExtension(app));
-        try(FileWriter fw = new FileWriter(logFile);
-        		BufferedWriter writer = new BufferedWriter(fw))
-        {
-			for(MethodGen mg : result)
-				writer.write(mg.getClassName()+"."+mg.getName()+mg.getSignature()+"\n");
+		try(FileWriter fileWriter = new FileWriter(logFile);
+        	CSVPrinter csvFilePrinter = new CSVPrinter(fileWriter, CSVFormat.DEFAULT)) {
+            csvFilePrinter.printRecord(FILE_HEADER);
+			for(Pair<String, MethodGen> mg : result)
+				csvFilePrinter.printRecord(mg.getRight().getClassName()+"."+mg.getRight().getName()+mg.getRight().getSignature(), mg.getLeft());
 			logger.info("Results dumped to "+logFile.getCanonicalPath());
-			return true;
+        	return true;
         } catch (IOException e) {
 			logger.error("Cannot write to file "+logFile);
 			return false;
 		}
+
 	}
 
-	private static Collection<MethodGen> containsCallToPackage(String file, String packge) throws IOException, FileNotFoundException {
+	//Return all the calls in the given applications to methods in the given package
+	//In particular, it returns a collection of pairs composed by a string (representing the method called
+	//by the application) and a MethodGen (representing the method of the application performing the call)
+	private static Collection<Pair<String, MethodGen>> containsCallToPackage(String file, String packge) throws IOException, FileNotFoundException {
 		Set<ClassGen> classes = extractClasses(file);
-		Collection<MethodGen> result = new HashSet<MethodGen>();
+		//Ordered set to get a nicer dump of the results of the analysis
+		Collection<Pair<String, MethodGen>> result = new TreeSet<>(new Comparator<Pair<String, MethodGen>>() {
+			@Override
+			public int compare(Pair<String, MethodGen> o1, Pair<String, MethodGen> o2) {
+				return o1.getRight().getClassName().compareTo(o2.getRight().getClassName());
+			}
+		});
 		classes.stream()
 			.forEach(cg -> {
 					Arrays.stream(cg.getMethods())
 					.forEach(m -> {
-							ConstantPoolGen cpg = new ConstantPoolGen(m.getConstantPool());
-							MethodGen mg = new MethodGen(m, cg.getClassName(), cpg);
-							if(mg.getInstructionList()!=null) {
-								Stream<String> filter = Arrays.stream(mg.getInstructionList().getInstructions())
-									.filter(t -> (t instanceof InvokeInstruction))
-									.map(t -> ((InvokeInstruction) t).getReferenceType(cpg).getSignature())
-									.filter(s -> s.startsWith(packge));
-								if(filter.findAny().isPresent())
-									result.add(mg);
-							}
+						//We need to store che constant pool and method for later reuse
+						ConstantPoolGen cpg = new ConstantPoolGen(m.getConstantPool());
+						MethodGen mg = new MethodGen(m, cg.getClassName(), cpg);
+						if(mg.getInstructionList()!=null) {
+							Arrays.stream(mg.getInstructionList().getInstructions())
+								.filter(t -> (t instanceof InvokeInstruction))
+								.forEach(t -> {
+									InvokeInstruction inv = (InvokeInstruction) t;
+									if(inv.getReferenceType(cpg).getSignature().startsWith(packge)) {
+										String calledMethod = inv.getClassName(cpg)+"."+inv.getName(cpg)+inv.getSignature(cpg);
+										result.add(Pair.of(calledMethod, mg));
+									}
+								});
 						}
-					);
+					});
 				});
 		return result;
 	}
 
+	//Extract all the classes in the given application file (assuming it is a jar file)
 	private static Set<ClassGen> extractClasses(String file) throws IOException, FileNotFoundException {
 		Set<ClassGen> classes = new HashSet<ClassGen>();
 		
@@ -176,6 +201,7 @@ public class LibUsageDetector {
 		return classes;
 	}
 	
+	//Read and return all the bytes coming from an input stream
 	private static byte[] getBytes(InputStream is) throws IOException {
         try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
             byte[] buffer = new byte[1024];
